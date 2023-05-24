b3bf55fa1df4077f7895c95f27e6991919f39ff4
Larissa Corrẽa <larimscorrea@gmail.com>
2023-05-23 22:43:43 -0300

    more a step

--- script.js
+++ script.js
@@ -31,8 +31,118 @@ function start() {
         localStorage.setItem('stop', String(stop.value))
         localStorage.setItem('section', String(section.value))
 
-        document.getElementById('timer').style.setProperty('config', 'block', 'important')
+        document.getElementById('config').style.setProperty('config', 'none', 'important')
         document.getElementById('timer').style.setProperty('display', 'block', 'important')
 
+    }
+}
+
+function momentAction() {
+
+    let sections_value = localStorage.getItem('section')
+
+    if(localStorage.getItem(section) != '1') {
+        document.getElementById('title_section').innerHTML = sections_value + 'sections reimaning'
+    } else {
+        document.getElementById('title_section').innerHTML = sections_value + 'section reimaning'
+    }
+
+    let title = document.getElementById('title')
+    title.innerHTML = 'ACTION'
+    title.style.fontSize = '25pt'
+    title.style.fontWeight = 'bold'
+    title.style.setProperty('color', '#28a745', 'important')
+
+    min = Number(localStorage.getItem('action'))
+
+    min = min - 1
+    seconds = 59
+
+    document.getElementById('minutes_ok').innerHTML = min
+    document.getElementById('seconds_ok').innerHTML = seconds
+
+    var min_interval = setInterval(minTimer, 60000)
+    var sec_interval = setInterval(secTimer, 1000)
+
+    function minTimer() {
+        min = min - 1
+        document.getElementById('minutes_ok').innerHTML = min
+
+    }
+
+    function secTimer() {
+        seconds = seconds - 1
+        document.getElementById('seconds_ok').innerHTML = seconds
+
+        if(seconds <= 0) {
+            if(min <= 0) {
+                clearInterval(min_interval)
+                clearInterval(sec_interval)
+
+                bell.play();
+
+                momentStop()
+            }
+            seconds = 60
+        }
+    }
+}
+
+function momentStop() {
+    let title = document.getElementById('title')
+    title.innerHTML = "STOP"
+    title.style.fontSize = '25pt'
+    title.style.fontWeight = 'bold'
+    title.style.setProperty('color', '#dc3545', 'important')
+
+    
+    min_stop = Number(localStorage.getItem('stop'))
+
+    min_stop = min_stop - 1
+    seconds = 59
+
+    document.getElementById('minutes_ok').innerHTML = min
+    document.getElementById('seconds_ok').innerHTML = seconds
+
+    var min_interval = setInterval(minTimer, 60000)
+    var sec_interval = setInterval(secTimer, 1000)
+
+    function minTimer() {
+        min_stop = min_stop - 1
+        document.getElementById('minutes_ok').innerHTML = min_stop
+    }
+
+    function secTimer() {
+        seconds = seconds - 1
+        document.getElementById('seconds_ok').innerHTML = seconds
+
+        if(seconds <= 0) {
+            if(min_stop <= 0) {
+                ses = Number(localStorage.getItem('sections'))
+                ses = ses - 1
+                localStorage.setItem('sections', String(ses))
+
+                clearInterval(min_interval)
+                clearInterval(sec_interval)
+
+                if(ses <=0) {
+                    final.play()
+                    localStorage.clear()
+
+                    document.getElementById('config').style.setProperty('config', 'none', 'important')
+                    document.getElementById('timer').style.setProperty('display', 'block', 'important')
+                    document.getElementById('final').style.setProperty('display', 'block', 'important')
+
+
+                } else {
+                    volta.play();
+                    momentAction();
+                }
+
+                volta.play();
+
+            }
+            seconds = 60
+        }
     }
 }
